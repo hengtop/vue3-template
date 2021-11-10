41d53923ba4bf5f54a7d6b5e91e6ef0c505e61bd
zhangheng <1515452760@qq.com>
2021-11-11 00:06:53 +0800

    fix(lint): test8

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,3 @@
-/*
- * @Date: 2021-11-11 00:01:18
- * @LastEditors: zhangheng
- * @LastEditTime: 2021-11-11 00:01:19
- */
-module.exports = { extends: ["@commitlint/config-conventional"] }
+module.exports = {
+  extends: ['@commitlint/config-conventional']
+}
