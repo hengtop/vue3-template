a843200d079b6bb08420924de9b3cf55e7162c14
zhangheng <1515452760@qq.com>
2021-11-11 00:08:11 +0800

    fix(lint): test9

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,9 @@
-import { createStore } from "vuex"
+/*
+ * @Date: 2021-11-10 23:23:08
+ * @LastEditors: zhangheng
+ * @LastEditTime: 2021-11-11 00:07:54
+ */
+import { createStore } from 'vuex'
 
 export default createStore({
   state: {},
