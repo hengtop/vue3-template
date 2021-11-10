ac3e26fd0e7da0d9eb22f5682cc425a23911584f
zhangheng <1515452760@qq.com>
2021-11-11 00:15:30 +0800

    fix(lint): test11

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
+<!--
+ * @Date: 2021-11-10 23:23:08
+ * @LastEditors: zhangheng
+ * @LastEditTime: 2021-11-11 00:15:05
+-->
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about 1</h1>
   </div>
 </template>
