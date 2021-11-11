58240122e296b5788ccb08f606dc0f15330b46d4
zhangheng <heng.top@qq.com>
2021-11-11 09:14:44 +0800

    feat(all): 完成基本的配置

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
+/*
+ * @Date: 2021-11-10 19:44:37
+ * @LastEditors: zhangheng
+ * @LastEditTime: 2021-11-11 08:53:29
+ */
 module.exports = {
   presets: ['@vue/cli-plugin-babel/preset']
-}
+};
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,8 @@
+/*
+ * @Date: 2021-11-10 20:23:02
+ * @LastEditors: zhangheng
+ * @LastEditTime: 2021-11-11 08:53:21
+ */
 module.exports = {
   extends: ['@commitlint/config-conventional']
-}
+};
